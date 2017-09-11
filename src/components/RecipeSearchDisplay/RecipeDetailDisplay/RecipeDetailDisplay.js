c4585c458c8b6461f89b2568f84eba3423f2995a
Ana <analujauregui@gmail.com>
2017-09-10 22:16:47 -0600

    Display detailed information from 2nd API call based on recipe selection

---
+++ src/components/RecipeSearchDisplay/RecipeDetailDisplay/RecipeDetailDisplay.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import RecipeSearchDisplay from '../'
+
+const RecipeDetailDisplay = () => {
+  return(
+    <div>
+      RECIPE DETAILS WILL SHOW HEREE!
+    </div>
+  )
+}
+
+export default RecipeDetailDisplay;
