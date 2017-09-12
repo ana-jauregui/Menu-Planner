926c5f4f33f55446570a1becdbc86cccab828cfd
Ana <analujauregui@gmail.com>
2017-09-11 20:40:16 -0600

    merge

--- src/components/RecipeSearchDisplay/RecipeSearchDisplay.js
+++ src/components/RecipeSearchDisplay/RecipeSearchDisplay.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
 // import { Redirect } from 'react-router-dom';
-
-import { Link } from 'react-router-dom';
-
 import MainContainerContainer from '../../containers/MainContainerContainer';
-import FullRecipeInformation from '../FullRecipeInformation/FullRecipeInformation';
 import './RecipeSearchDisplay.css';
 
 export class RecipeSearchDisplay extends Component {
@@ -17,44 +13,42 @@ export class RecipeSearchDisplay extends Component {
   }
 
   render() {
+    // console.log('recipeSearchDisplay', this.props);
 
-    // if(this.props.recipeSearchData.isSearchComplete === true && this.props.recipeDetailData.recipeDetailRequested === false) {
+    if(this.props.recipeSearchData.isSearchComplete === true && this.props.recipeDetailData.recipeDetailRequested === false) {
 
       const { recipeId ,recipeName, image, time, rating } = this.props
 
       return(
-        <Link to='/recipe-details' >
-          <div onClick={() => this.viewRecipeDetail(recipeId)}>
-            <p>{ recipeName }</p>
-            <img src={ image }/>
-            <p>{`Rating: ${rating}`}</p>
-            <p>{`Cook Time: ${time/60} min`}</p>
-          </div>
-        </Link>
-      )
-    // }
-
-    if(this.props.recipeSearchData.isSearchComplete === true && this.props.recipeDetailData.recipeDetailRequested === true) {
-    //
-    //   const { name, totalTime, numberOfServings, ingredientLines } = this.props.recipeDetailData.recipeDetailData
-    //   const image = this.props.recipeDetailData.recipeDetailData.images[0].hostedLargeUrl
-    //   const sourceUrl = this.props.recipeDetailData.recipeDetailData.source.sourceRecipeUrl
-    //   const ingredients = ingredientLines.map((ingredient, i) => <ul key={i}><li>{ingredient}</li></ul>)
-    //
-    //   console.log(this.props.recipeDetailData.recipeDetailData);
-      return(
-        <div>
-          <FullRecipeInformation />
+        <div onClick={() => this.viewRecipeDetail(recipeId)}>
+          <p>{ recipeName }</p>
+          <img src={ image }/>
+          <p>{`Rating: ${rating}`}</p>
+          <p>{`Cook Time: ${time/60} min`}</p>
         </div>
-    //     <div>{this.props.recipeId === this.props.recipeDetailData.recipeDetailData.id ? <div><div>
-    //       <p>{name}</p>
-    //       <img src={image} />
-    //       <button onClick={() => window.open(sourceUrl)}>View Instructions</button>
-    //       <p>Total Cook Time: {totalTime}</p>
-    //       <p>Number of Servings: {numberOfServings}</p>
-    //     </div>{ingredients}</div>: null}</div>
       )
     }
+
+    if(this.props.recipeSearchData.isSearchComplete === true && this.props.recipeDetailData.recipeDetailRequested === true) {
+        // <Redirect to='/recipe-details'/>
+
+      const { name, totalTime, numberOfServings, ingredientLines } = this.props.recipeDetailData.recipeDetailData
+      const image = this.props.recipeDetailData.recipeDetailData.images[0].hostedLargeUrl
+      const sourceUrl = this.props.recipeDetailData.recipeDetailData.source.sourceRecipeUrl
+      const ingredients = ingredientLines.map((ingredient, i) => <ul key={i}><li>{ingredient}</li></ul>)
+
+      console.log(this.props.recipeDetailData.recipeDetailData);
+      return(
+        <div>{this.props.recipeId === this.props.recipeDetailData.recipeDetailData.id ? <div><div>
+        <p>{name}</p>
+        <img src={image} />
+        <button onClick={() => window.open(sourceUrl)}>View Instructions</button>
+        <p>Total Cook Time: {totalTime}</p>
+        <p>Number of Servings: {numberOfServings}</p>
+       </div>{ingredients}</div>: null}</div>
+      )
+    }
+
   }
 }
 
