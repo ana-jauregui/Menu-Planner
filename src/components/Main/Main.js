c3f4d1ce4adfea008d62f5e29091d347f7e41d81
Ana <analujauregui@gmail.com>
2017-09-11 21:39:10 -0600

    view

---
+++ src/components/Main/Main.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import MainContainerContainer from '../../containers/MainContainerContainer';
+import RecipeSearchDisplay from '../RecipeSearchDisplay/RecipeSearchDisplay';
+import './MainContainer.css'
+
+
+export class Main extends Component {
+
+  render() {
+    if(this.props)
+
+
+    return(
+      <div className='recipe-search-display-container'>
+
+      </div>
+    )
+
+}
+
+export default MainContainerContainer(MainContainer);
